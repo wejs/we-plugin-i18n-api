d782e5dfa00129e32bdf4c3542e3a55b98d67a09
albertosouza <alberto.souza.99@gmail.com>
2019-02-19 00:20:09 -0300

    improve api return with we.js 2.1 i18n feature

--- plugin.js
+++ plugin.js
@@ -2,14 +2,9 @@
  * We.js i18n plugin api
  * Add routes for get and set translations in one we.js project
  */
-const path = require('path');
 
 module.exports = function loadPlugin(projectPath, Plugin) {
   const plugin = new Plugin(__dirname);
-  // set plugin configs
-  // plugin.setConfigs({
-  // });
-
   // plugin routes:
   plugin.setRoutes({
     'get /i18n/get-all-locales': {
@@ -24,20 +19,8 @@ module.exports = function loadPlugin(projectPath, Plugin) {
     // controllers:
     we.controllers['i18n-api'] = new we.class.Controller({
       getAllLocales(req, res) {
-        const localesFolder = path.join(process.cwd(), 'config', 'locales');
-        // get all locale files:
-        req.we.utils.listFilesRecursive(localesFolder, (err, files)=> {
-          const locales = {};
-
-          req.we.utils.async
-          .eachSeries(files, (file, next)=> {
-            const nameParts = file.replace('.json', '').split('/');
-            locales[nameParts[nameParts.length-1]] = require(file);
-            next();
-          }, function afterLoadAll() {
-            res.status(200).send({ locales: locales });
-          });
-        });
+        const locales = we.i18n.getCachedTranslations();
+        res.status(200).send({ locales: locales });
       }
     });
 
